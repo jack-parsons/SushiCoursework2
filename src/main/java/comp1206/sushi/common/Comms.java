5d34ea6ae50e31c3982152eb85f0c8eeeb618e90
Jack Parsons <jack.parsons.uk@gmail.com>
2019-04-03 00:08:09 +0100

    initial Comms

---
+++ src/main/java/comp1206/sushi/common/Comms.java
@@ -0,0 +1,14 @@
+package comp1206.sushi.common;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Comms {
+    private final static int PORT = 1357;
+    private Socket socket;
+
+    Comms() {
+
+    }
+}
